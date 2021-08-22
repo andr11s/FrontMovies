11d3ed79f8db4fd3c1f811a32e2bcd661540a1fa
andr11s <alvisandres1@gmail.com>
2021-08-22 00:42:46 -0500

    feact: agregue ruta inicial del proyecto en inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './angularmaterial.module';
+import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-Co' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
